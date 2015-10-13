46e6f6c18d9bef3d4fae45ce27fc2bd9c1a8f2ee
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-10-13 17:25:59 +0100

    Add more detail to DatabaseCache

--- annotateVCF.py
+++ annotateVCF.py
@@ -4,13 +4,29 @@ import unittest
 
 class DatabaseCache(object):
   def __init__(self, database_dir):
-    pass
+    self.database_dir = database_dir
+    self.create_database_dir_if_missing()
+    self.create_config_yaml_if_missing()
+    self.update_snpeff_config()
 
   def get_database(self, gff_file):
-    pass
+    config = self.load_config()
+    database_checksum = self.get_md5(gff_file)
+    try:
+      database_name = config['databases'][database_checksum]
+      database = AnnotationDatabase.load(database_name, self.database_dir)
+    except ValueError:
+      database = self._add_database(gff_file)
+      config['databases'][database_checksum] = database.name
+      self.save_config(config)
+      self.update_snpeff_config()
+    return database
 
-  def add_database(self, gff_file):
-    pass
+  def _add_database(self, gff_file):
+    database_name = self._get_database_name(gff_file)
+    database = AnnotationDatabase.create(database_name, self.database_dir,
+                                         gff_file)
+    return database
 
 class AnnotationDatabase(object):
   pass
@@ -18,10 +34,7 @@ class AnnotationDatabase(object):
 if __name__ == '__main__':
   args = parse_arguments()
   database_cache = DatabaseCache(args.database_dir)
-  try:
-    database = database_cache.get_database(args.gff)
-  except MissingDatabase:
-    database = database_cache.add_database(args.gff)
+  database = database_cache.get_database(args.gff)
   contigs_in_common = database.check_contigs(args.input_vcf)
   annotated_vcf = database.annotate_vcf(args.input_vcf)
   annotation_errors = check_annotated_vcf(annotated_vcf)
