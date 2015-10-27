81286b4baac48cec197e3f12ade2eb6d48040b81
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-10-27 17:39:04 +0000

    Add TravisCI

---
+++ install_dependencies.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+set -e
+
+start_dir=$(pwd)
+
+SNPEFF_VERSION="v4_1l_core"
+SNPEFF_DOWNLOAD_URL="http://sourceforge.net/projects/snpeff/files/snpEff_${SNPEFF_VERSION}.zip"
+
+# Make an install location
+if [ ! -d "${HOME}/dependencies" ]; then
+  mkdir ${HOME}/dependencies
+fi
+build_dir="${HOME}/dependencies"
+
+# DOWNLOAD ALL THE THINGS
+download () {
+  url=$1
+  download_location=$2
+
+  if [ -e $download_location ]; then
+    echo "Skipping download of $url, $download_location already exists"
+  else
+    echo "Downloading $url to $download_location"
+    wget $url -O $download_location
+  fi
+}
+
+cd $build_dir
+download $SNPEFF_DOWNLOAD_URL snpEff_${SNPEFF_VERSION}.zip
+unzip snpEff_${SNPEFF_VERSION}.zip
+mv snpEff snpEff_${SNPEFF_VERSION}
+
+export SNPEFF_EXEC="${build_dir}/snpEff_${SNPEFF_VERSION}/snpEff.jar"
+
+cd $start_dir
+
+set +x
+set +e
