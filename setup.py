148cd1e7959b2dca0efb8eac4306f16418ac1207
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-10-29 18:06:47 +0000

    Close annotated VCF
    NFS didn't like trying to delete a folder with a file still open :(

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import multiprocessing
 
 setup(name='snpEffWrapper',
-      version='0.2.0',
+      version='0.2.1',
       scripts=[
         'scripts/snpEffBuildAndRun'
       ],
