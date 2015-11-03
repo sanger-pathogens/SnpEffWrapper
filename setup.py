b5db48c6cb02b284d30f27dafbc715cf7540a4b4
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-11-03 09:57:43 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import multiprocessing
 
 setup(name='snpEffWrapper',
-      version='0.2.1',
+      version='0.2.2',
       scripts=[
         'scripts/snpEffBuildAndRun'
       ],
